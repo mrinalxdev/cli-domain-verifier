04c7dd7408252e88d0755f8c630f721d246dadae
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-09 13:05:47 +0530

    Added Data Updated Readme File

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CLI Email verifier
 
 a small tool to check whether the email is verfied or not .. Not Production Level ready but a small project for learning 
+
--- main.go
+++ main.go
@@ -37,4 +37,33 @@ func checkDomain (domain string){
 	if len(mxRecords) > 0 {
 		hasMX = true
 	}
+
+	txtRecords, err  := net.LookupTXT(domain)
+
+	if err!= nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range txtRecords{
+		if strings.HasPrefix(record, "v=spf1"){
+			hasSPF = true
+			spfRecord = record
+
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1"){
+			hasDMARC = true
+			dmarcRecord = record
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
 }
